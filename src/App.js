11f30e86831a6c47b0fe0470bb84fd607ea6f343
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-05-13 16:55:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+
+  const [hasPhoto, setHasPhoto] = useState(false);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1920, height: 1080 }
+      })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16 / 9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext('2d');
+    ctx.drawImage(video, 0, 0, width, height)
+    setHasPhoto(true);
+  }
+
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext('2d')
+
+    ctx.clearReact(0, 0, photo.width, photo.height)
+
+    setHasPhoto(false)
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>SNAP!</button>
+      </div>
+      <div className={"result " + (hasPhoto ? "hasPhoto" : "")}>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>CLOSE!</button>
+      </div>
     </div>
   );
 }
